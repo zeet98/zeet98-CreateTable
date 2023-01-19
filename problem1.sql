0d0a91c3d8d4d81aa91abed82689d965fdbdb840
tedbeast <theodore.balashov@revature.com>
2023-01-19 15:26:30 -0500

    added helpful comments

---
+++ problem1.sql
@@ -0,0 +1,1 @@
+write your sql statement in a single line in the problem1.sql file.
--- src/main/java/CreateATable.java
+++ src/main/java/CreateATable.java
@@ -61,8 +61,12 @@ public class CreateATable {
      *     |'Let it be'        |'Beatles'              |
      *     |'Hotel California' |'Eagles'               |
      *     |'Kashmir'          |'Led Zeppelin'         |
+     *
+     *     Do not change anything in this code. You should write your sql statement on a single line in the
+     *     problem1.sql file.
      */
     public void problem1(){
+//        write your SQL logic in a single line in the problem1.sql file.
         String sql = FileUtil.parseSQLFile("problem1.sql");
 
         try {
